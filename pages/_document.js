08a03871f195d3e3af40b4bb0e52ad586b523dd3
vaibhavcrane <vaibhavcrane@me.com>
2022-10-10 13:06:17 -0400

    removed google font

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html>
-			<Head>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700|Poppins:wght@300;400;500;600;700'
-					rel='stylesheet'
-				/>
-			</Head>
+			<Head></Head>
 			<body>
 				<Main />
 				<NextScript />
