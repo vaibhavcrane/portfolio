458b9bcce15d5c1417111a98defbc5abd84c549a
vaibhavcrane <vaibhavcrane@me.com>
2022-10-11 17:30:19 -0400

    added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+const NotFound = () => {
+	return (
+		<div className='intro error_page'>
+			<div
+				className='hero'
+				style={{
+					backgroundImage: `url(${"img/404/background.png"})`,
+				}}>
+				<div className='content'>
+					<h1>404!</h1>
+					<p>The page you are looking for could not be found.</p>
+
+					<Link className='white-fill-bg btn-outline' href={"/"}>
+						BACK TO HOME
+					</Link>
+					{/* End purchase_button */}
+				</div>
+			</div>
+			{/* End .hero */}
+		</div>
+	);
+};
+
+export default NotFound;
