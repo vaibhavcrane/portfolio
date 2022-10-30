a36a2762f2af376e6ff848c848c0d7a911c5b51b
vaibhavcrane <vaibhavcrane@me.com>
2022-10-30 13:09:11 -0400

    visual changes

--- pages/breaking-bad/index.js
+++ pages/breaking-bad/index.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Head from "next/head";
-import BreakingBadSlider from "../../components/breaking-bad-slider";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
 
 export default function Profile() {
+	const particlesInit = useCallback(async (engine) => {
+		await loadFull(engine);
+	}, []);
+
+	const particlesLoaded = useCallback(async (container) => {}, []);
 	const [data, setData] = useState(null);
 	const [isLoading, setLoading] = useState(false);
 
@@ -27,11 +33,126 @@ export default function Profile() {
 				<title>Breaking Bad Quotes for D</title>
 			</Head>
 			<div className='home'>
-				<BreakingBadSlider />
-				<div className='container'>
-					<div className='brba-quotes'>
-						<h1>{data[0].quote}</h1>
-						<h2>- {data[0].author}</h2>
+				<div className='slider'>
+					<div className='hero breaking-bad' data-style='one'>
+						<div className='frame-layout__particles'>
+							<Particles
+								id='tsparticles'
+								init={particlesInit}
+								loaded={particlesLoaded}
+								options={{
+									backgroundMode: {
+										enable: true,
+										zIndex: 1,
+									},
+									fullScreen: {
+										enable: false,
+										zIndex: -1,
+									},
+									fpsLimit: 60,
+									interactivity: {
+										detectsOn: "canvas",
+										events: {
+											onClick: {
+												enable: false,
+												mode: "push",
+											},
+											onHover: {
+												enable: false,
+												mode: "repulse",
+											},
+											resize: true,
+										},
+										modes: {
+											bubble: {
+												distance: 400,
+												duration: 2,
+												opacity: 0.4,
+												size: 40,
+											},
+											push: {
+												quantity: 4,
+											},
+											repulse: {
+												distance: 200,
+												duration: 0.4,
+											},
+										},
+									},
+									particles: {
+										color: {
+											value: "#ddd",
+										},
+										links: {
+											color: "#ffffff",
+											distance: 150,
+											enable: true,
+											opacity: 0.5,
+											width: 1,
+										},
+										collisions: {
+											enable: true,
+										},
+										move: {
+											direction: "none",
+											enable: true,
+											outMode: "bounce",
+											random: false,
+											speed: 2,
+											straight: false,
+										},
+										number: {
+											density: {
+												enable: true,
+												value_area: 800,
+											},
+											value: 25,
+										},
+										opacity: {
+											value: 0.5,
+										},
+										shape: {
+											type: "circle",
+										},
+										size: {
+											random: true,
+											value: 6,
+										},
+									},
+
+									detectRetina: true,
+								}}
+							/>
+						</div>
+						<div className='background'>
+							<div
+								className='image'
+								style={{
+									backgroundImage: `url(${"/img/breaking-bad/background.jpg"})`,
+								}}></div>
+						</div>
+						{/* End .background */}
+
+						<div className='container'>
+							<div className='content'>
+								<div className='name_wrap'>
+									<h2>
+										<span>{data[0].quote}</span>
+									</h2>
+								</div>
+								{/* End title */}
+
+								<div className='job_wrap'>
+									<span className='job'>
+										- {data[0].author}
+										{/* <span className='overlay_effect'></span> */}
+									</span>
+								</div>
+								{/* End designation */}
+							</div>
+							{/* End content */}
+						</div>
+						{/* End .container */}
 					</div>
 				</div>
 			</div>
