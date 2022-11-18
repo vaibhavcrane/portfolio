20d0049b16f759786ae70545b116b5267b9ebc55
vaibhavcrane <vaibhavcrane@me.com>
2022-11-17 20:28:11 -0500

    added meta tags

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -7,6 +7,7 @@ import {
 	useTransform,
 	MotionValue,
 } from "framer-motion";
+import Head from "next/head";
 
 function useParallax(value: MotionValue<number>, distance: number) {
 	return useTransform(value, [0, 1], [-distance, distance]);
@@ -34,19 +35,25 @@ const Gallery = () => {
 	});
 
 	return (
-		<div className='scroll-snap all-wrap gallery-wrapper'>
-			{/* <Header /> */}
-			{/* <Slider page_title='Gallery' /> */}
-			{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((image) => (
-				<ImageOnScreen key={image} id={image} />
-			))}
-			<motion.div className='progress' style={{ scaleX }} />
-			<div className='gallery-button'>
-				<a className='white-fill-bg btn-outline' href={"/"}>
-					BACK TO HOME
-				</a>
+		<>
+			<Head>
+				<title>Vaibhav Rane |Gallery</title>
+				<meta name='description' content="Vaibhav Rane's Photo Gallery" />
+			</Head>
+			<div className='scroll-snap all-wrap gallery-wrapper'>
+				{/* <Header /> */}
+				{/* <Slider page_title='Gallery' /> */}
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((image) => (
+					<ImageOnScreen key={image} id={image} />
+				))}
+				<motion.div className='progress' style={{ scaleX }} />
+				<div className='gallery-button'>
+					<a className='white-fill-bg btn-outline' href={"/"}>
+						BACK TO HOME
+					</a>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
